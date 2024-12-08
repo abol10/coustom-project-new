e9d93e6ff47023e63d396a85573112dc32c08818
neon <nono@gmail.com>
2024-12-08 15:11:21 +0330

    update font size

--- c-p/tailwind.config.js
+++ c-p/tailwind.config.js
@@ -16,6 +16,17 @@ export default {
         jigary:' hsla(354, 56%, 42%, 1)',
       }
     },
+    fontSize: {
+      ff:'8px',
+      xs:'0.75rem',
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
   },
   plugins: [],
 }
