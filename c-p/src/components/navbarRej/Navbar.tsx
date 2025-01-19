b013be183b607f7e93d4359aa9edea620cf8764a
neon <nono@gmail.com>
2025-01-20 00:48:04 +0330

    update Navbar

--- c-p/src/components/navbarRej/Navbar.tsx
+++ c-p/src/components/navbarRej/Navbar.tsx
@@ -1,12 +1,32 @@
 import { FaUser, FaPhone } from "react-icons/fa";
 import { Link, useLocation } from 'react-router-dom';
+import { MdOutlineDriveFolderUpload } from "react-icons/md";
+import { BsShop } from "react-icons/bs";
+
+
 
 const Navbarrej = () => {
   // استفاده از useLocation برای دریافت مسیر فعلی
   const location = useLocation();
 
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-evenly">
+      <Link 
+        to="/N" 
+        className={`grid justify-center text-primary-400 ml-20 mb-5 ${location.pathname.toLowerCase() === `${"/n"}` ? 'text-red-500' : ''}`}
+      >
+        <MdOutlineDriveFolderUpload className="ml-6" />
+        <span className="block text-ff mt-2">ثبت نمونه کار</span>
+      </Link>
+      <img className="w-36 h-2 mt-3 ml-10" src="img/Line 38.png" alt="خط جدا کننده" />
+      <Link 
+        to="/F" 
+        className={`grid justify-center text-primary-400 ml-20 mb-5 ${location.pathname.toLowerCase() === `${"/f"}` ? 'text-red-500' : ''}`}
+      >
+        <BsShop className="ml-6" />
+        <span className="block text-ff mt-2">اطلاعات فروشگاه</span>
+      </Link>
+      <img className="w-36 h-2 mt-3 ml-10" src="img/Line 38.png" alt="خط جدا کننده" />
       {/* لینک اول */}
       <Link 
         to="/E" 
@@ -27,6 +47,7 @@ const Navbarrej = () => {
         <FaUser className="ml-6" />
         <span className="block text-ff mt-2">اطلاعات کاربر</span>
       </Link>
+    
     </div>
   );
 }
