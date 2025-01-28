cd50b13e6f17ac6cef817e27635d6ca1ddfa2506
neon <nono@gmail.com>
2025-01-28 12:00:42 +0330

    update Not pages

--- c-p/src/components/else/else.tsx
+++ c-p/src/components/else/else.tsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import Hede from "../pages1/Hehe";
+
 const Else = () => {
+    const naviget = useNavigate();
     return ( 
-        <div>
-            صحفه موجود نیست
+        <div className="relative ">
+            <Hede />
+         <img className=" h-96 w-4/5 rounded-xl ml-32" src="img/error404.png" alt="" />
+         <h1 className="font-bold text-xl text-center mb-10 absolute left-2/4 ">صحفه مورد نظر یافت نشد</h1>
+         <button onClick={()=>naviget('/')} className="btn btn-error text-white text-ff text-center absolute left-2/4 -bottom-24 ">برگشت به صحفه اصلی</button>
         </div>
      );
 }
