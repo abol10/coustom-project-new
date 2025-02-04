d4730f8dd8e67882cac7f62802c9d6ffe9a30c90
neon <nono@gmail.com>
2025-02-04 16:18:54 +0330

    update App

--- c-p/src/App.tsx
+++ c-p/src/App.tsx
@@ -8,6 +8,7 @@ import MyForm from './components/Registration/Registration';
 import Myetalat from './components/etlaeattnas/Etlauat';
 import Nmonekar from './components/nmonekar/Nmonekar';
 import Etluate_Shop from './components/etluate_shop/etluat_shop';
+import PrifilePage1 from './components/ProfilePage/Profilepage1';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/S" element={<MyForm />} />
           <Route path="/E" element={<Myetalat />} />
           <Route path="/N" element={<Nmonekar />} />
+          <Route path="/Profile" element={<PrifilePage1 />} />
           <Route path="/F" element={<Etluate_Shop />} />
           {/* مسیر پیش‌فرض */}
           <Route path="*" element={<Else />} />
